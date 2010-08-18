18a365afee11043a99cc222eb340a6f3abd77fc8
Don Cruse <doncruse@gmail.com>
2010-08-18 01:49:53 -0500

    Added places; need smart quotes and to finish the full list

--- citation-form.rb
+++ citation-form.rb
@@ -93,8 +93,119 @@ TABLE6 = {
     "development" => "Dev.",
     "developments" => "Devs.",
     "director" => "Dir.",
-    "directors" => "Dirs."
+    "directors" => "Dirs.",
+    "discount" => "Disc.",
+    "discounts" => "Discs.",
+    "distributor" => "Distrib.",
+    "distributors" => "Distribs.",
+    "distributing" => "Distrib.",
+    "district" => "Dist.",
+    "districts" => "Dists.",
+    "division" => "Div.",
+    "divisions" => "Divs.",
+    "east" => "E.",
+    "eastern" => "E.",
+    "economic" => "Econ.",
+    "economics" => "Econ.",
+    "economical" => "Econ.",
+    "economy" => "Econ.",
+    "economies" => "Econs.",
+    "education" => "Educ.",
+    "educational" => "Educ.",
+    "electric" => "Elec.",
+    "electrical" => "Elec.",
+    "electricity" => "Elec.",
+    "electronic" => "Elec.",
+    "electronics" => "Elecs.",
+    "engineer" => "Eng'r",
+    "engineers" => "Eng'rs",
+    "engineering" => "Eng'g",
+    "enterprise" => "Enter.",
+    "enterprises" => "Enters.",
+    "entertainment" => "Entm't",
+    "entertainments" => "Entm'ts",
+    "environment" => "Env't",
+    "environments" => "Env'ts",
+    "environmental" => "Envtl.",
+    "equality" => "Equal.",
+    "equipment" => "Equip.",
+    "equipments" => "Equips.",
+    "examiner" => "Exam'r",
+    "examiners" => "Exam'rs",
+    "exchange" => "Exch.",
+    "exchanges" => "Exchs.",
+    "executor" => "Ex'r",
+    "executrix" => "Ex'x",
+    "executors" => "Ex'rs",
+    "executrixes" => "Ex'xs",
+    "export" => "Exp.",
+    "exports" => "Exps.",
+    "federal" => "Fed.",
+    "federals" => "Feds."
 }
+  # These abbreviations validated against the 18th edition of the Bluebook.
+  # No claim of ownership is made about any abbrevation, because that
+  # would be silly.
+  
+  PLACES = {
+  	'alabama' => 'Ala.',
+  	'american aamoa' => 'Am. Sam.',
+  	'arizona' => 'Ariz.',
+  	'arkansas' => 'Ark.',
+  	'baltimore' => 'Balt.',
+  	'california' => 'Cal.',
+  	'chicago' => 'Chi.',
+  	'colorado' => 'Colo.',
+  	'connecticut' => 'Conn.',
+  	'delaware' => 'Del.',
+  	'district of columbia' => 'D.C.',
+  	'florida' => 'Fla.',
+  	'georgia' => "Ga.",
+  	'hawaii' => 'Haw.',
+  	'hawai\'i' => 'Haw.',
+  	'illinois' => 'Ill.',
+  	'indiana' => 'Ind.',
+  	'kansas' => 'Kan.',
+  	'kentucky' => "Ky.",
+  	'los angeles' => "L.A.",
+  	'louisiana' => "La.",
+  	'maine' => 'Me.',
+  	'maryland' => 'Md.',
+  	'massachusetts' => 'Mass.',
+  	'michigan' => "Mich.",
+  	'minnesota' => 'Minn.',
+  	'mississippi' => 'Miss.',
+  	'missouri' => 'Mo.',
+  	'montana' => 'Mont.',
+  	'nebraska' => 'Neb.',
+  	'nevada' => 'Nev.',
+  	'new hampshire' => 'N.H.',
+  	'new jersey' => 'N.J.',
+  	'new mexico' => 'N.M.',
+  	'new york' => 'N.Y.',
+  	'north carolina' => 'N.C.',
+  	'north dakota' => 'N.D.',
+  	'northern mariana islands' => 'N. Mar. I.',
+  	'oklahoma' => 'Okla.',
+  	'oregon' => 'Or.',
+  	'pennsylvania' => 'Pa.',
+  	'philadelphia' => 'Phila.',
+  	'puerto rico' => 'P.R.',
+  	'rhode island' => 'R.I.',
+  	'south carolina' => 'S.C.',
+  	'south dakota' => 'S.D.',
+  	'san francisco' => 'S.F.',
+  	'tennessee' => 'Tenn.',
+  	'texas' => 'Tex.',
+  	'vermont' => 'Vt.',
+  	'virgin islands' => 'V.I.',
+  	'virginia' => 'Va.',
+  	'washington' => 'Wash.',
+  	'west virginia' => 'W. Va.',
+  	'wisconsin' => 'Wis.',
+  	'wyoming' => 'Wyo.'
+  }
+  
 
 ###########################################################
 # Yes, this is the whole program.
@@ -108,6 +219,10 @@ TABLE6 = {
 # titlecase routine, such as the one published
 # by John Gruber.
 
+# NOTE: After I have developed this, I may think
+# about some speed optimizations, such as merging these
+# into a single hash rather than separated by type.
+
 casename = STDIN.read
 casename.strip!
 tmp = Array.new
@@ -115,6 +230,8 @@ tmp = Array.new
 casename.split(/\s/).each do |word|
   if TABLE6.has_key?(word.downcase)
     tmp << TABLE6[word.downcase].capitalize
+  elsif PLACES.has_key?(word.downcase)
+    tmp << PLACES[word.downcase].capitalize    
   else
     tmp << word
   end
